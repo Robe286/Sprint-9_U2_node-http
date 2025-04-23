254cc5cdc450404102b0ad0c0b8f59079ba1b3f0
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-23 18:09:02 +0200

    creación archivos y objeto

---
+++ data.js
@@ -0,0 +1,17 @@
+// Este objeto podría incluir propiedades como title, subtitle y description. Puedes añadir las que quieras y los valores puedes inventártelos.
+const urlDescription = 'https://es.wikipedia.org/wiki/The_Matrix'
+
+function information () {
+    return {
+        Title: 'Matrix',
+        Year: '1999',
+        Subtitle: 'Ciencia-Ficcion',
+        Description: `${urlDescription}`,
+        Direction: 'Hermanas Wachouski',
+        Budget: 'USD 63.000.000',
+        Collection: 'USD 467.841.735',
+    }
+
+}
+
+console.log(information())
